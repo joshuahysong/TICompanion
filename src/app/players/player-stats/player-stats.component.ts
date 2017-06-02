90d80c1cd9f331167b3a2acc3087fe9b931658b7
Joshua Hysong <joshuahysong@gmail.com>
2017-06-02 07:28:55 -0400

    Stats table added

--- src/app/players/player-stats/player-stats.component.ts
+++ src/app/players/player-stats/player-stats.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
+import { Race } from 'app/shared/race.model';
+import { RaceService } from 'app/shared/race.service';
 import { Player } from '../shared/player.model';
 import { PlayerService } from '../shared/player.service';
+import { Unit } from 'app/shared/unit.model';
+import { Units } from 'assets/units';
 
 @Component({
   selector: 'app-player-stats',
@@ -10,16 +14,26 @@ import { PlayerService } from '../shared/player.service';
   styleUrls: ['./player-stats.component.css']
 })
 export class PlayerStatsComponent implements OnInit {
-  @Input() player: Player;
-  @Input() test: number;
+  player: Player;
+  races: Race[];
+  units;
 
   constructor(
+    private playerService: PlayerService,
+    private raceService: RaceService,
     private route: ActivatedRoute,
-    private router: Router,
-    private playerService: PlayerService
+    private router: Router
   ) { }
 
   ngOnInit() {
+    // subscribe to router event
+    this.route.parent.params.subscribe((params: Params) => {
+        let id = params['id'];
+        this.player = this.playerService.getPlayer(id);
+      });
+
+    this.races = this.raceService.getRaces();
+    this.units = Units;
 
   }
 }
