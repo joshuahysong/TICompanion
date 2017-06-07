0c5141c48a1866844038e814d70cff4c2b5fb03e
Joshua Hysong <joshuahysong@gmail.com>
2017-06-07 08:49:01 -0400

    Player unit-test tweaks

--- src/app/players/player/player.component.spec.ts
+++ src/app/players/player/player.component.spec.ts
@@ -1,6 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { PlayerComponent } from './player.component';
+import { Race } from 'app/shared/race.model';
+import { RaceService } from 'app/shared/race.service';
+import { Player } from '../shared/player.model';
+import { PlayerService } from '../shared/player.service';
+import { Unit } from 'app/shared/unit.model';
+import { Units } from 'assets/units';
+
+@Component({
+  template: ''
+})
+class DummyComponent {
+}
 
 describe('PlayerComponent', () => {
   let component: PlayerComponent;
@@ -8,7 +24,12 @@ describe('PlayerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlayerComponent ]
+      imports: [ FormsModule,
+        RouterTestingModule.withRoutes([
+            { path: 'players/:id/stats', component: DummyComponent }
+          ]) ],
+      declarations: [ PlayerComponent, DummyComponent ],
+      providers: [ PlayerService, RaceService ]
     })
     .compileComponents();
   }));
