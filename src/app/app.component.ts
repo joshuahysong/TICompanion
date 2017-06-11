9054e95789e8fe716032a72d6fd164ed8d5566a9
joshuahysong <joshuahysong@gmail.com>
2017-06-11 18:56:38 -0400

    Fixed dynamic subdirectory for local dev vs build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+
+if (environment.production) {
+  var urlProduction = null;
+  urlProduction = '/TICompanion';
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  url = urlProduction; // {{url}} in the template.
+}
