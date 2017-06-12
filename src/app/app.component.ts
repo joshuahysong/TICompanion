ebc06bb3c67ab28580e7aa29340db259565d9df7
Joshua Hysong <joshuahysong@gmail.com>
2017-06-12 13:18:11 -0400

    Images in navigation will use correct baseurl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
 import { environment } from '../environments/environment';
 
-if (environment.production) {
-  var urlProduction = null;
-  urlProduction = '/TICompanion';
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  url = urlProduction; // {{url}} in the template.
-}
+export class AppComponent {}
